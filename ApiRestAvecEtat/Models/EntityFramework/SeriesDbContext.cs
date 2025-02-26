f4b6114b6aa1c406db797be195b993c73b85ccb8
melih0132 <melih.cetinkaya.32@gmail.com>
2025-02-26 23:55:31 +0100

    Ajoutez des fichiers projet.

---
+++ ApiRestAvecEtat/Models/EntityFramework/SeriesDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiRestAvecEtat.Models.EntityFramework;
+
+public partial class SeriesDbContext : DbContext
+{
+    public SeriesDbContext()
+    {
+    }
+
+    public SeriesDbContext(DbContextOptions<SeriesDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Serie> Series { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Server=localhost;port=5433;Database=SeriesDB; uid=postgres; password=postgres;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Serie>(entity =>
+        {
+            entity.HasKey(e => e.Serieid).HasName("serie_pkey");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
